db1247d0bc3f8c44ba29fe20c0780f481b985dd5
Abdurrahman Köken <abdurrahman@akoken.com>
2017-02-24 14:26:45 +0300

    update producer app.

--- src/Kafka.ProducerApp/Program.cs
+++ src/Kafka.ProducerApp/Program.cs
@@ -9,16 +9,16 @@ namespace Kafka.ProducerApp
     {
         static void Main(string[] args)
         {
-            var options = new KafkaOptions(new Uri("http://localhost:9092/"));
+            var options = new KafkaOptions(new Uri("http://192.168.0.1:9092/"), new Uri("http://192.168.0.1:9093/"), new Uri("http://192.168.0.1:9094/"));
+
             using (var router = new BrokerRouter(options))
             {
                 using (var client = new Producer(router))
                 {
-                    client.SendMessageAsync("TestTopic", new[] { new Message("Hello world!") }).Wait();
-                    Console.WriteLine("Message has been sent..");
+                    client.SendMessageAsync("test-topic", new[] { new Message("Hello World!") }).Wait();
+                    Console.WriteLine("Message has been sent.");
                 }
             }
-            Console.ReadKey();
         }
     }
 }
