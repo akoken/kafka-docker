711fb1cc488ed66eae96dd3e217026cfe3efcfc1
Abdurrahman Köken <abdurrahman@akoken.com>
2017-02-24 14:26:34 +0300

    update consumer app.

--- src/Kafka.ConsumerApp/Program.cs
+++ src/Kafka.ConsumerApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using KafkaNet;
 using KafkaNet.Model;
+using KafkaNet.Protocol;
 using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace Kafka.ConsumerApp
@@ -9,17 +11,18 @@ namespace Kafka.ConsumerApp
     {
         static void Main(string[] args)
         {
-            var options = new KafkaOptions(new Uri("http://localhost:9092/"));
+            var options = new KafkaOptions(new Uri("http://192.168.0.1:9092/"), new Uri("http://192.168.0.1:9093/"), new Uri("http://192.168.0.1:9094/"));
+
             using (var router = new BrokerRouter(options))
             {
-                using (var consumer = new Consumer(new ConsumerOptions("TestTopic", router)))
+                using (var consumer = new Consumer(new ConsumerOptions("test-topic", router)))
                 {
-                    foreach (var message in consumer.Consume())
+                    IEnumerable<Message> messages = consumer.Consume();
+                    foreach (var message in messages)
                     {
                         Console.WriteLine("Partition Id:{0}, Offset:{1}, Message:{2}",
                             message.Meta.PartitionId, message.Meta.Offset, Encoding.UTF8.GetString(message.Value));
                     }
-                    Console.ReadKey();
                 }
             }
         }
